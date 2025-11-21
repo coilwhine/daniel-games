5c86d3e3a0ac2da37af889bd1d256a8e261c1ab4
coilwhine <hen.daniel47@gmail.com>
2025-11-22 01:16:08 +0200

    header takes to homepage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Header.scss";
+import { useNavigate } from "react-router";
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
+  const navigate = useNavigate();
+
   return (
-    <header className="Header">
+    <header className="Header" onClick={() => navigate("/")}>
       <h1>ביגבגים משחקים</h1>
     </header>
   );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { Link } from "react-router";
 import "./HomePage.scss";
-import { Link, NavLink } from "react-router";
 
 interface HomePageProps {}
 
